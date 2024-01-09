f10a2e09c25c0b78509d9fa39b698efd5a81c0af
JoaquimManhoca <joaquimmanhoca222@gmail.com>
2024-01-09 13:33:43 +0200

    feat : criou-se a function pin_verification onde contem todas as condicoes posiveis informando ao usuario se o pin foi verificado ou não

--- src/script.js
+++ src/script.js
@@ -1,6 +1,24 @@
 const input_data = document.querySelector('#input_user_data');
 const submit_data = document.querySelector('#submit_user_data');
 
-submit_data.addEventListener('click', ()=>{
-    console.log(input_data.value);
-})
+submit_data.addEventListener('click', pin_verification)
+
+function pin_verification() {
+    const data = input_data.value.length;
+    if ( data > 15) {
+        console.log('PIN não verificado');
+        console.log(`o seu PIN contem ${data} números, deve ser muito Menor`);
+    }
+    else if ( data > 8) {
+        console.log('PIN não verificado');
+        console.log(`o seu PIN contem ${data} números, deve ser Menor`);
+    }
+    else if (data >= 4) {
+        console.log('PARABÉNS');
+        console.log('PIN verificado com sucesso');
+    }
+    else {
+        console.log('PIN não verificado');
+        console.log(`o seu pIN contem ${data} números, deve ser Maior no minimo 4 números`);
+    }
+}
